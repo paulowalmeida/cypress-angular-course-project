62d607d375745ce330d301b85fe79129ac6ec14c
Paulo W. A. Ferreira <weskleyalmeida.si@gmail.com>
2022-07-30 19:27:29 -0300

    create a register.cy file into new dir alurapic

---
+++ cypress/e2e/alurapic/register.cy.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+describe('Register scenarios', () => {
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com/');
+        cy.contains('a[href="#/home/signup"]', 'Register now').click();
+    });
+
+    context('should display warning messages when', () => {
+        beforeEach(() => {
+            cy.contains('button', 'Register').click();
+        });
+
+        it('inputs not filled', () => {
+            cy.contains('button', 'Register').click();
+            cy.contains('ap-vmessage > small.text-danger', 'Email is required').should('be.visible');
+            cy.contains('ap-vmessage > small.text-danger', 'Full name is required!').should('be.visible');
+            cy.contains('ap-vmessage > small.text-danger', 'User name is required!').should('be.visible');
+            cy.contains('ap-vmessage > small.text-danger', 'Password is required').should('be.visible');
+        });
+
+        it('a invalid email is entered', () => {
+            cy.get('input[formcontrolname="email"]').type('test');
+            cy.contains('ap-vmessage > small.text-danger', 'Invalid e-mail').should('be.visible');
+        });
+
+        it('a name is little than 2 characters', () => {
+            cy.get('input[formcontrolname="fullName"]').type('a').blur();
+            cy.contains('ap-vmessage > small.text-danger', 'Mininum length is 2').should('be.visible');
+        });
+
+        it('a username is little than 2 characters', () => {
+            cy.get('input[formcontrolname="userName"]').type('a').blur();
+            cy.contains('ap-vmessage > small.text-danger', 'Mininum length is 2').should('be.visible');
+        });
+
+        it('length of password is little than 8 characters', () => {
+            cy.get('input[formcontrolname="password"]').type('test').blur();
+            cy.contains('ap-vmessage > small.text-danger', 'Mininum length is 8').should('be.visible');
+        });
+
+        it('length of password is bigger than 18 characters', () => {
+            cy.get('input[formcontrolname="password"]').type('abcdefghijklmnopqrstuvwxyz').blur();
+            cy.contains('ap-vmessage > small.text-danger', 'Maximun length is 18').should('be.visible');
+        });
+    });
+});
