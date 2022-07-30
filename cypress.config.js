edf1868f7dea2fe24df452d2aabc0b28c4608cd9
Paulo W. A. Ferreira <weskleyalmeida.si@gmail.com>
2022-07-30 20:05:28 -0300

    configure screenshots on the project

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,18 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      on('after:spec', (spec, results) => {
+        if (results && results.spec) {
+          // Do we have failures for any retry attempts?
+          const failures = _.some(results.tests, (test) => {
+            return _.some(test.attempts, { state: 'failed' })
+          })
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            return del(results.spec)
+          }
+        }
+      })
     },
   },
 });
